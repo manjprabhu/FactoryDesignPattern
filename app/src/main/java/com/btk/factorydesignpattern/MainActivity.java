5d6c7c6a23febe43b404e8148980893b1195fbb7
MPB <mp4learning.com>
2020-07-21 21:04:20 +0530

    Updated commit

--- app/src/main/java/com/btk/factorydesignpattern/MainActivity.java
+++ app/src/main/java/com/btk/factorydesignpattern/MainActivity.java
@@ -12,6 +12,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
 
         Shape shape = ShapeFactory.getShape("rectangle");
